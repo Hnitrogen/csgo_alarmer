08d9272e1aa04bc18d7bd989187dd3ba1b448a8a
Hnitrogen <1791422575@qq.com>
2024-05-16 20:09:31 +0800

    Update buff.py

--- buff.py
+++ buff.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 
+#
 cookies = os.getenv("Cookie")
 
 headers = {
@@ -21,62 +22,57 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
 }
 
-params1 = {
+params = {
     'game': 'csgo',
     'force': '0',
     'page_num': '1',
     'page_size': '50',
     'search': '',
+    'sort_by': 'price.desc',
     'state': 'all',
-    'rarity': 'ancient_weapon',
-    '_': '1715748102081',
+    '_': '1715860208100',
 }
 
-params2 = {
-    'game': 'csgo',
-    'force': '0',
-    'page_num': '1',
-    'page_size': '50',
-    'search': '',
-    'state': 'all',
-    'rarity': 'legendary_weapon',
-    '_': '1715748102081',
-}
+# 我没有steam开放商密钥，但是Buff有啊hhh
+response = requests.get('https://buff.163.com/api/market/steam_inventory', params=params,headers=headers)
 
-line_list = []
+if response.text.find("<title>登录</title>") > 0:
+    # 说明Cookie失效了 --- 发送更新Cookie的邮件（10天更新一次）
+    with open("res.html", 'w') as file:
+        file.write("Cookie过期,请手动更新secrets的Cookie")
+else:
+    json_resp = json.loads(response.text)
+    res_list = json_resp["data"]["items"]
 
+    line_list = []
+    for item in res_list:
+        price = item.get('sell_min_price', 0)
+        if float(price) < 50:
+            continue        # 过滤便宜的枪
 
-def crawler_buff(params_in):
-    # 我没有steam开放商密钥，但是Buff有啊hhh
-    response = requests.get('https://buff.163.com/api/market/steam_inventory', params=params_in, headers=headers)
+        tradable_text = item.get("tradable_text", "可交易")
+        name = item.get("name", "商品名获取失败")
+        icon_url = item.get("icon_url", "图片链接获取失败")
 
-    if response.text.find("<title>登录</title>") > 0:
-        # 说明Cookie失效了 --- 发送更新Cookie的邮件（10天更新一次）
-        with open("res.html", 'w') as file:
-            file.write("Cookie过期,请手动更新secrets的Cookie")
-    else:
-        json_resp = json.loads(response.text)
-        res_list = json_resp["data"]["items"]
+        line = f"""
+            <img style="width: 100px ; height: 100px" src="{icon_url}">
+            <div style="font-weight: bolder;color: red">{name}</div>
+            <div>{tradable_text}</div>
+        """
+        line_list.append(line)
 
-        for item in res_list:
-            tradable_text = item.get("tradable_text", "可交易")
-            name = item.get("name", "商品名获取失败")
-            icon_url = item.get("icon_url", "图片链接获取失败")
+    line_list.insert(0, """
+        <!DOCTYPE html>
+                <html lang="">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+                    <link rel="icon" href="<%= BASE_URL %>favicon.ico">
+                    <title><%= htmlWebpackPlugin.options.title %></title>
+                  </head>
+                  <body>""")
+    line_list.append("  </body> </html>")
 
-            line = f"""
-                <img style="width: 100px ; height: 100px" src="{icon_url}">
-                <div style="font-weight: bolder;color: red">{name}</div>
-                <div>{tradable_text}</div>
-            """
-            line_list.append(line)
-
-        line_list.insert(0, "<!DOCTYPE html><html lang=''><head><meta charset='utf-8'></head><body>")
-        line_list.append("</body></html>")
-
-
-if __name__ == "__main__":
-    crawler_buff(params1)
-    crawler_buff(params2)
-
-    with open("res.html", 'a') as file:
+    with open("res.html", 'w') as file:
         file.writelines(line_list)
